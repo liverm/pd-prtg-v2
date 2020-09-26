1032155bc9e664eb3c1c0db29ac3a9bf21844223
Matt Livermore <ml@ebcue.plus.com>
2020-09-26 12:17:42 +0100

    Added script and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # prtg2pd
+
+## Test Environment
+Windows Server 2016 Standard
+PRTG v19.2
+PowerShell v5.x or higher
+
+## Requirement
+Send a JSON-formatted alert payload to a PagerDuty Events v2 API endpoint managed by an event ruleset.
+
+## Installation
+Copy the PowerShell script to the following directory on the PRTG server:
+
+C:\Program Files (x86)\PRTG Network Monitor\Notifications\EXE
+
+The script is designed to send events to PagerDuty that will both trigger and resolve an incident.  Modify the script adding the appropriate PagerDuty integration key.  To minimize the number of scripts that need to be maintained, consider using a key associated with an event ruleset endpoint.
+
+The script is called via a new Notification Template (see Setup | Account Settings | Notification Templates).
+
+Then select the 'Execute Program' method and select the corresponding PowerShell script for creating and resolving PagerDuty incidents.
+
+Specify the parameters PRTG should send to the PowerShell script.  The script expects the following parameters in the exact order:
+
+`'%probe' '%device' '%deviceid' '%sensor' '%sensorid' '%group' '%groupid' '%home' '%host' '%status' '%colorofstate' '%down' '%priority' '%message' '%comments' '%datetime'`
+
+Then create a Notification Trigger.  This can be done in one of two ways:
+
+1. Create a new Library and drag the desired devices into the Library and then create the Notification Trigger at this level.
+1. Create the Notification Trigger at the Root level.
+
+## Futher Optimisations Within PagerDuty
+The final thing I would suggest is that you then enable alert grouping on your services.
+
+This will then group all the alerts for a given node into a single incident.
---
+++ prtg2pd.ps1
@@ -0,0 +1,105 @@
+# Powershell script to trigger and resolve PRTG alerts in PagerDuty using the events v2 API
+
+# Notes
+# Copy the line below (exluding the hash) into the parameters field in the PRTG notification template
+# '%probe' '%device' '%deviceid' '%sensor' '%sensorid' '%group' '%groupid' '%home' '%host' '%status' '%colorofstate' '%down' '%priority' '%message' '%comments' '%datetime'
+
+# Ingest the alert payload from PRTG
+Param(
+    [string]$probe,
+    [string]$device,
+    [string]$deviceid,
+    [string]$sensor,
+    [string]$sensorid,
+    [string]$group,
+    [string]$groupid,
+    [string]$prtg_home,
+    [string]$prtg_host,
+    [string]$status,
+    [string]$colorofstate,
+    [string]$down,
+    [string]$priority,
+    [string]$message,
+    [string]$comments,
+    [string]$datetime
+)
+
+# Determine the Event Action
+$regex = [regex] "\((.*)\)"
+$action = $regex::match($status, $regex).groups[1]
+
+switch ($action) {
+    "now: Up" { $Event = "resolve" }
+    default { $Event = "trigger" }
+}
+
+# Determine the Severity
+switch ($colorofstate) {
+    "#b4cc38"	{ $Severity = "info" }
+    "#ffcb05"	{ $Severity = "warning" }
+    #   "Error"		{$Severity="error"}
+    "#d71920"	{ $Severity = "critical" }
+    default { $Severity = "critical" }
+}
+
+$RoutingKey = "3534011a3b6d450a8ae967b57f6ef48a"
+$Url = "https://events.pagerduty.com/v2/enqueue"
+
+$Description = "$device $sensor $status $down"
+# $Severity = "critical"
+$Timestamp = Get-Date -UFormat "%Y-%m-%dT%T%Z"
+
+$AlertPayload = @{
+    routing_key  = $RoutingKey
+    event_action = $Event
+    dedup_key    = $sensor
+    client       = "PRTG Network Monitor"
+    client_url   = $prtg_home
+    payload      = @{
+        summary        = $Description
+        timestamp      = $Timestamp
+        source         = $device
+        severity       = $Severity
+        component      = $group
+        class          = $sensor
+        custom_details = @{
+            prtg_server  = $prtg_home
+            probe        = $probe
+            group        = $group, $groupid
+            device       = $device, $deviceid
+            sensor       = $sensor, $sensorid
+            url          = $prtg_host
+            colorofstate = $colorofstate
+            down         = $down
+            downtime     = $downtime
+            priority     = $priority
+            message      = $message
+            datetime     = $datetime
+            comments     = $comments
+            status       = $status
+        }
+    }
+}
+
+# Convert Events Payload to JSON
+
+$json = ConvertTo-Json -InputObject $AlertPayload
+
+$logEvents = "C:\pagerduty\logs\prtg2pd_log.txt"
+
+# Send to PagerDuty and Log Results
+
+$LogMtx = New-Object System.Threading.Mutex($False, "LogMtx")
+$LogMtx.WaitOne() | Out-Null
+
+try {
+    Invoke-RestMethod	-Method Post `
+        -ContentType "application/json" `
+        -Body $json `
+        -Uri $Url `
+    | Out-File $logEvents -Append
+}
+
+finally {
+    $LogMtx.ReleaseMutex() | Out-Null
+}
